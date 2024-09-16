5f08564262ce11fd9fde0cd26ce290a7af27cbb0
sneha-chavan <sneha-manohar.chavan@capgemini.com>
2024-09-16 13:47:33 +0530

    conflicting main & master branch resolved

---
+++ .next/static/webpack/app/layout.63586bdc5d7fc4f9.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/layout",{
+
+/***/ "(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22C%3A%5C%5CUsers%5C%5Csnehamch%5C%5CProjects%5C%5CNextJS%5C%5Cnextjs-dashboard%5C%5Capp%5C%5Cui%5C%5Cglobal.css%22%2C%22ids%22%3A%5B%5D%7D&server=false!":
+/*!*******************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22C%3A%5C%5CUsers%5C%5Csnehamch%5C%5CProjects%5C%5CNextJS%5C%5Cnextjs-dashboard%5C%5Capp%5C%5Cui%5C%5Cglobal.css%22%2C%22ids%22%3A%5B%5D%7D&server=false! ***!
+  \*******************************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./app/ui/global.css */ \"(app-pages-browser)/./app/ui/global.css\"));\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL25vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvYnVpbGQvd2VicGFjay9sb2FkZXJzL25leHQtZmxpZ2h0LWNsaWVudC1lbnRyeS1sb2FkZXIuanM/bW9kdWxlcz0lN0IlMjJyZXF1ZXN0JTIyJTNBJTIyQyUzQSU1QyU1Q1VzZXJzJTVDJTVDc25laGFtY2glNUMlNUNQcm9qZWN0cyU1QyU1Q05leHRKUyU1QyU1Q25leHRqcy1kYXNoYm9hcmQlNUMlNUNhcHAlNUMlNUN1aSU1QyU1Q2dsb2JhbC5jc3MlMjIlMkMlMjJpZHMlMjIlM0ElNUIlNUQlN0Qmc2VydmVyPWZhbHNlISIsIm1hcHBpbmdzIjoiQUFBQSxnS0FBaUgiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLz9hM2FiIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCgvKiB3ZWJwYWNrTW9kZTogXCJlYWdlclwiICovIFwiQzpcXFxcVXNlcnNcXFxcc25laGFtY2hcXFxcUHJvamVjdHNcXFxcTmV4dEpTXFxcXG5leHRqcy1kYXNoYm9hcmRcXFxcYXBwXFxcXHVpXFxcXGdsb2JhbC5jc3NcIik7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22C%3A%5C%5CUsers%5C%5Csnehamch%5C%5CProjects%5C%5CNextJS%5C%5Cnextjs-dashboard%5C%5Capp%5C%5Cui%5C%5Cglobal.css%22%2C%22ids%22%3A%5B%5D%7D&server=false!\n"));
+
+/***/ })
+
+});
